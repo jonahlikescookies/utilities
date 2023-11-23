96696f735d22766f2a51be45d27351282bd1acf9
jonahlikescookies <146877361+jonahlikescookies@users.noreply.github.com>
2023-11-23 14:20:47 -0800

    my regex sucks

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function changecolor(hex){
   const ishex = /#([0-9a-f]{6}|[0-9a-f]{3})/i;
-  if (hex.test(ishex)) {
+  if (ishex.test(hex)) {
     document.getElementById("preview").style.fill = hex;
   }
   else alert("Invalid hexidecimal code(maybe you forgot the #?)");
