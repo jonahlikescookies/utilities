b15768ecc2bcf9d23dad0320a4985381fc241c05
jonahlikescookies <146877361+jonahlikescookies@users.noreply.github.com>
2023-11-23 15:09:22 -0800

    seperate color change function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function changecolor(hex){
+function changehex(hex){
   const ishex = /#([0-9a-f]{6}|[0-9a-f]{3})/i;
-  if (ishex.test(hex)) {
-    document.getElementById("preview").style.fill = hex;
-  }
+  if (ishex.test(hex)) document.getElementById("preview").style.fill = hex;
   else alert("Invalid hexidecimal code(maybe you forgot the #?)");
+}
+function changergb(r, g, b){
+  document.getElementById("preview").style.fill = rgb(r, g, b);
 }
